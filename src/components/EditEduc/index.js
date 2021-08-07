4cd65affb541bec9b3d1e1e1c2e1fe26a9e95215
thiago-porto25 <thiago_porto25@hotmail.com>
2021-08-07 11:47:38 -0300

    initiate work on edit components

---
+++ src/components/EditEduc/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function EditEduc() {
+  return <div>Editing Educ</div>
+}
---
+++ src/components/EditSkills/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function EditSkills() {
+  return <div>Editing Skills</div>
+}
