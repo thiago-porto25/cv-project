7130f9e694516b198481a03e2c3b3685b5dcf5ba
thiago-porto25 <thiago_porto25@hotmail.com>
2021-08-12 17:34:54 -0300

    add global styles

---
+++ src/GlobalStyles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  html, body, #root {
+    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    background-color: #ffffff;
+    color: white;
+    font-size: 16px;
+    margin: 0;
+    height: 100vh;
+  }
+`
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,51 +3,92 @@ import { Header, Footer, CvItem } from '../components'
 import styled from 'styled-components'
 import DataContext from '../context/DataContext'
 
-const EmptyMessage = styled.div``
+const CvsContainer = styled.div`
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+  grid-template-columns: 1fr 1fr;
+`
+
+const EmptyMessage = styled.h1``
 
 const HomeBg = styled.div`
-  background-color: #fff000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #010048;
+  height: 100%;
 `
-const HomeContainer = styled.div``
 
-const NewCVButton = styled.button``
+const HomeContainer = styled.div`
+  width: 80%;
+  height: 90%;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`
+
+const NewCVButton = styled.button`
+  width: 14rem;
+  height: 3rem;
+  color: white;
+  font-size: 20px;
+  font-weight: bold;
+  background-color: #03002a;
+  border: none;
+  border-radius: 10px;
+  box-shadow: 0px 0px 5px black;
+  cursor: pointer;
+  box-sizing: border-box;
+  transition: 100ms ease-in-out;
+
+  &:hover {
+    background-color: #010057;
+    transform: scale(1.05);
+  }
+`
 
 export default function Home(props) {
   const { data, setCurrentCv, setCurrentCvIndex } = useContext(DataContext)
 
   return (
-    <HomeBg>
+    <>
       <Header {...props} />
-      <HomeContainer>
-        {!data ? (
-          <EmptyMessage>
-            It seems you have no CVs. Create you first one!
-          </EmptyMessage>
-        ) : (
-          data.map((item, i) => (
-            <CvItem
-              key={`${item} - ${i}`}
-              data-id={i}
-              itemData={item}
-              onClick={() => {
-                props.setInHome(false)
-                props.setInPreview(true)
-                setCurrentCv(item)
-                setCurrentCvIndex(i)
-              }}
-            />
-          ))
-        )}
-        <NewCVButton
-          onClick={() => {
-            props.setInHome(false)
-            props.setInCreating(true)
-          }}
-        >
-          NEW CV
-        </NewCVButton>
-      </HomeContainer>
+      <HomeBg>
+        <HomeContainer>
+          <CvsContainer>
+            {!data ? (
+              <EmptyMessage>
+                It seems you have no CVs. Create you first one!
+              </EmptyMessage>
+            ) : (
+              data.map((item, i) => (
+                <CvItem
+                  key={`${item} - ${i}`}
+                  data-id={i}
+                  itemData={item}
+                  onClick={() => {
+                    props.setInHome(false)
+                    props.setInPreview(true)
+                    setCurrentCv(item)
+                    setCurrentCvIndex(i)
+                  }}
+                />
+              ))
+            )}
+          </CvsContainer>
+          <NewCVButton
+            onClick={() => {
+              props.setInHome(false)
+              props.setInCreating(true)
+            }}
+          >
+            CREATE A NEW CV
+          </NewCVButton>
+        </HomeContainer>
+      </HomeBg>
       <Footer />
-    </HomeBg>
+    </>
   )
 }
